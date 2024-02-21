2f347e8950c84402022433ea975fc293d94c56f1
saty4a <satyabratbiswal5413@gmail.com>
2024-02-21 13:34:07 +0530

    first commit

--- src/components/homePageHeader.jsx
+++ src/components/homePageHeader.jsx
@@ -4,15 +4,15 @@ import { VscBellDot } from "react-icons/vsc";
 const HomePageHeader = () => {
   return (
     <div className="homepage-header-animation animate slide-in flex flex-col items-center lg:flex-row gap-5">
-      <p className="text-white mt-5 flex gap-2 lg:flex-col font-normal text-3xl mx-5">
+      <p className="text-white mt-5 flex gap-2 lg:flex-col font-normal text-3xl mx-5 lg:me-0">
         Hello
         <span className="font-extrabold">
-          Dr.Colter! <span className="waving-hand">👋🏻</span>
+          Dr.Colter! 👋🏻<span className="waving-hand"></span>
         </span>
       </p>
       <input
         type="text"
-        className="bg-[#2C3A58] rounded-2xl w-[20rem] md:w-[36rem] py-3 ps-4 mt-4 ms-5 lg:ms-0"
+        className="bg-[#2C3A58] rounded-2xl w-[20rem] md:w-[33rem] py-3 ps-4 mt-4"
         placeholder=" 🔍 search"
       />
       <div className="bell-icon bg-[#409BEE] mb-5 me-10 mt-4 ms-5 lg:mb-0 lg:ms-0 hover:border hover:border-2 hover:border-white">
