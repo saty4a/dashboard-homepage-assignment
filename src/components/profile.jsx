8d8c7880ff10de6b2a085306eb8d3b21788965ed
saty4a <satyabratbiswal5413@gmail.com>
2024-02-21 13:49:19 +0530

    first commit

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -8,26 +8,27 @@ import ChatCard from "./chatCard";
 import CircularStyle from "./circularStyle";
 
 const Profile = () => {
-  const avatars = [aliza, maira, alexa, "8+"];
+    const avatars = [
+        aliza,
+        maira,
+        alexa,
+        "8+"
+      ]
 
-  return (
-    <div className="bg-[#111729] animate profile-animation rounded-3xl mx-5 2xl:me-[3rem] px-5 mt-[3.2rem] my-10 flex flex-col items-center h-[62.2rem] gap-8">
-      <CircularStyle
-        classStyle={"profile-circle"}
-        container={"profile-container"}
-        image={person}
-      />
-      <div className="text-center">
-        <h4 className="font-bold text-2xl">ALIAM COLTER</h4>
-        <p className="font-medium text-base text-[#CCCCCC]">Physician</p>
-      </div>
-      <div className="bg-[#1C2A4E] flex justify-between items-center w-full md:w-2/3 lg:w-full rounded-3xl px-5 py-5">
-        <p className="font-semibold text-lg">Active Patients</p>
-        <AvatarStack avatars={avatars} />
-      </div>
-      <ChatCard />
-    </div>
-  );
-};
+    return(
+        <div className="bg-[#111729] animate profile-animation rounded-3xl mx-5 px-5 my-10 flex flex-col items-center 2xl:me-[3rem] h-[60.5rem] gap-8">
+            <CircularStyle classStyle={"profile-circle"} container={"profile-container"} image={person} />
+            <div className="text-center">
+            <h4 className="font-bold text-2xl">ALIAM COLTER</h4>
+            <p className="font-medium text-base text-[#CCCCCC]">Physician</p>
+            </div>
+            <div className="bg-[#1C2A4E] flex justify-between items-center w-full md:w-2/3 lg:w-full rounded-3xl px-5 py-5">
+                <p className="font-semibold text-lg">Active Patients</p>
+                <AvatarStack avatars={avatars} />
+            </div>
+            <ChatCard />
+        </div>
+    )
+}
 
 export default Profile;
